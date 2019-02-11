d5d8c618fefca6c382f4077e228fb012baf7e43b
Nicole D <nnardella@gmail.com>
2019-02-11 18:22:42 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,4 +3,15 @@ class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
 
+  def slug
+   artist_name = self.name
+   artist_name.downcase.split(" ").join("-")
+ end
+
+ def self.find_by_slug(slug)
+   artist_name = slug.split("-").map(&:capitalize).join(' ')
+   artist = self.find_by(name: artist_name)
+   artist
+ end
+
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,4 +2,17 @@ class Genre < ActiveRecord::Base
   has_many :song_genres
   has_many :songs, through: :song_genres
   has_many :artists, through: :songs
+
+
+  def slug
+   genre_name = self.name
+   genre_name.downcase.split(" ").join("-")
+  end
+
+  def self.find_by_slug(slug)
+   genre_name = slug.split("-").map(&:capitalize).join(' ')
+   genre = self.find_by(name: genre_name)
+   genre
+  end
+
 end
