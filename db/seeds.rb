d59e0d3d8c85b3d0ecda59bae5d67d5fd044846a
Nicole D <nnardella@gmail.com>
2019-02-11 18:53:52 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,9 @@
 # Add seed data here. Seed your database with `rake db:seed`
 
-happy = Song.create(name: "Happy")
-june = Song.create(name: "Hey June")
+maroon = Artist.create(name: "Maroon 5")
+pearl_jam = Artist.create(name: "Pearl Jam")
+taylor_swift = Artist.create(name: "Taylor Swift")
+
+girls_like_you = Song.create(name: "Girls Like You", artist_id: maroon )
+alive = Song.create(name: "Alive", artist_id: pearl_jam)
+shake_it_off = Song.create(name: "Shake it off", artist_id: taylor_swift)
