60d4700aed87d55c972c9608a933ddeb9a603303
Nicole D <nnardella@gmail.com>
2019-02-11 18:23:37 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,4 +4,16 @@ class Song < ActiveRecord::Base
   has_many :song_genres
   has_many :genres, through: :song_genres
 
+  def slug
+   song_name = self.name
+   song_name.downcase.split(" ").join("-")
+ end
+
+ def self.find_by_slug(slug)
+   song_name = slug.split("-").map(&:capitalize).join(' ')
+   song = self.find_by(name: song_name)
+   song
+ end
+
+
 end
