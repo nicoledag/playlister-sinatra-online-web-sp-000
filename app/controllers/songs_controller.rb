8e86d9451b81623efd9001000493d4eb97cd8415
Nicole D <nnardella@gmail.com>
2019-02-12 17:50:05 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,10 +8,33 @@ use Rack::Flash
     erb :"songs/index"
   end
 
-  get '/songs/:slug' do
+  get "/songs/new" do
+     erb :"songs/new"
+   end
 
+   post "/songs" do
+     binding.pry
 
+    @artist = Artist.find_by(name: params["Artist Name"])
+    if !!@artist
+      @song = Song.create(name: params[:Name])
+      @song.artist = @artist
+      # @genre = Genre.find_by(name: params[:genres])
+      # @song.genres << @genre
+    else
+      @artist = Artist.create(name: params["Artist Name"])
+      @song = Song.create(name: params[:Name])
+      @song.artist = @artist
+      # @genre = Genre.find_by(name: params[:genres])
+      # @song.genres << @genre
+    end
   end
+
+
+  get "/songs/:slug" do
+   @song = Song.find {|song| song.slug == params[:slug]}
+   erb :"songs/show"
+ end
 
 
 end
